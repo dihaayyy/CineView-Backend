4ef78c88d302bd20949b7eb2ad7bafea5005d299
dihaayyy <148746377+dihaayyy@users.noreply.github.com>
2025-05-28 01:57:00 +0700

    Added db folder, db.js, and updating server.js

---
+++ db/db.js
@@ -0,0 +1,19 @@
+// db.js
+const mongoose = require("mongoose");
+const { mongoURI } = require("../config/config");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
+const connectDB = require("./db/db");
 
 const authRoutes = require("./routes/auth.routes");
 const movieRoutes = require("./routes/movie.routes");
@@ -9,19 +10,10 @@ const movieRoutes = require("./routes/movie.routes");
 const app = express();
 const port = process.env.port || 3000;
 
-let movies = [];
+connectDB();
 
 app.use(express.json());
 app.use(cors());
-
-mongoose
-  .connect(process.env.MONGO_URI, {})
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", movieRoutes);

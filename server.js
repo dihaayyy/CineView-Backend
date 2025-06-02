59480fa41b166c9684c4c9dad32c20bd918408a9
dihaayyy <148746377+dihaayyy@users.noreply.github.com>
2025-06-02 15:33:16 +0700

    Updating server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require("./db/db");
 
 const authRoutes = require("./routes/auth.routes");
 const movieRoutes = require("./routes/movie.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 const port = process.env.port || 3000;
@@ -17,6 +18,7 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", movieRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${port}`);

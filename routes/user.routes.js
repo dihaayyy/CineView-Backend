0e514860fa91858b2634e32a9151d46471fabc29
dihaayyy <148746377+dihaayyy@users.noreply.github.com>
2025-06-04 01:26:35 +0700

    Fixing routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/user.controller"); // Adjust the path as necessary
 const verifyToken = require("../middlewares/auth.middleware"); // Adjust the path as necessary
 
-router.get("/", userController.getUserById); // Get all users
-router.get("/:id", userController.getUserById); // Get user by ID
+router.get("/all", userController.getUserById); // Get all users
 router.get("/profile", verifyToken, userController.getLoggedInUserProfile); // Get logged-in user profile
+router.get("/:id", userController.getUserById); // Get user by ID
 
 module.exports = router;

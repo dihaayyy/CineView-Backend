62cec6b5f7248ecfcb852a3e91e611e6e79ab604
dihaayyy <148746377+dihaayyy@users.noreply.github.com>
2025-06-05 15:33:58 +0700

    Movie routes update

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -6,6 +6,7 @@ router.post("/", movieController.addMovie);
 router.get("/", movieController.getAllMovies);
 router.get("/:id", movieController.getMoviebyId);
 router.put("/:id", movieController.updateMoviebyId);
+router.put("/:id/poster", movieController.updatePosterUrl);
 router.delete("/:id", movieController.deleteMoviebyId);
 router.post("/:id/rate", movieController.rateMovie);
 router.post("/:id/comment", movieController.commentOnMovie);

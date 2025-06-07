4fe43c5d3c8bf15144d7333529cb74b444229483
dihaayyy <148746377+dihaayyy@users.noreply.github.com>
2025-06-07 20:17:21 +0700

    Fixing getAllUser routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/user.controller"); // Adjust the 
 const verifyToken = require("../middlewares/auth.middleware"); // Adjust the path as necessary
 
 router.get("/profile", verifyToken, userController.getLoggedInUserProfile); // Get logged-in user profile
-router.get("/all", userController.getUserById); // Get all users
+router.get("/all", userController.getAllUsers); // Get all users
 router.get("/:id", verifyToken, userController.getUserById); // Get user by ID
 router.get("/:id/favorites", verifyToken, userController.getFavoriteMovies); // Get user's favorite movies
 router.post("/:id/favorites", verifyToken, userController.addFavoriteMovie); // Add favorite movie to user
